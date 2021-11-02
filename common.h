0981a73f3e1e1bf6034011834d0ab05e0fb0c2cf
MihLeonid <mihleonid@gmail.com>
2021-11-02 17:21:25 +0300

    Add common and graph

---
+++ common.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "bits/stdc++.h"
+#include "MyStrategy.hpp"
+using namespace model;
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define INF 100000000000000LL
+template<typename T> bool rmax(T& a, T b); // a=max(a, b); return 1, if there was replace, and 0 if not
+template<typename T> bool rmin(T& a, T b); // Analog
+template<typename T> T mabs(T a); // Math absolute funcition
+
+// Template realization
+template<typename T> T mabs(T a){
+	return(a<0?-a:a);
+};
+template<typename T> bool rmin(T& a, T b){
+	if(b<a){
+		a=b;
+		return 1;
+	}
+	return 0;
+};
+template<typename T> bool rmax(T& a, T b){
+	if(a<b){
+		a=b;
+		return 1;
+	}
+	return 0;
+};
---
+++ graph.h
@@ -0,0 +1,16 @@
+#include "common.h"
+#pragma once
+namespace graph{
+	int line_dist(int from, int to); // Length of line (x+y)
+	int any_dist(int from, int to); // Length of path for any specialization
+	int any_next(int from, int to); // Number of next planet for any specialization
+	int fast_dist(int from, int to); // Length of path for logistics specialization
+	int fast_next(int from, int to); // Number of next planet for logistics specialization
+	void init_graph(const Game& g); // Call it just once in the begining of game
+}
+using graph::line_dist;
+using graph::any_dist;
+using graph::fast_dist;
+using graph::any_next;
+using graph::fast_next;
+using graph::init_graph;

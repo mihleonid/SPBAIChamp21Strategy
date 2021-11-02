34a4a3be957b4a90497a174ebbff3cac6704435e
MihLeonid <mihleonid@gmail.com>
2021-11-02 21:01:08 +0300

    Update GW

--- strategy/GameWrapper.cpp
+++ strategy/GameWrapper.cpp
@@ -6,22 +6,40 @@ GW::GW(){
 }
 void GW::update(const Game& g){
 	game=&g;
+	int N=g.planets.size();
 	if(!inited){
 		init();
 		inited=1;
 	}
+	if(get_tick()<3){
+		return; // See todo.txt for comments
+	}
 	ourScore=theirScore=0;
 	for(int i=0;i<g.players.size();++i){
-		(g.players[i].teamIndex-get_our_team()?theirScore:ourScore)+=g.players[i].score;
-		if(g.players[i].specialty){
-			(g.players[i].teamIndex-get_our_team()?whoCanEvil:whoCan)[g.players[i].specialty.value()]=i;
-		}
+		availFly[i]=game->maxFlyingWorkerGroups;
+		(is_player_evil(i)?theirScore:ourScore)+=g.players[i].score;
+		assert(g.players[i].specialty);
+		(is_player_evil(i)?whoCanEvil:whoCan)[g.players[i].specialty.value()]=i;
 	}
-	if(g.players[get_my_index()].specialty){
-		mySpecialty=g.players[get_my_index()].specialty.value();
+	for(auto fwg:g.flyingWorkerGroups){
+		int idx=fwg.playerIndex;
+		assert(availFly[idx]);
+		--availFly[idx];
+		(is_player_evil(idx)?theirFlyingBots:ourFlyingBots)[get_specialty(idx)]+=fwg.number;
+	}
+	for(int i=0;i<N;++i){
+		resources[i]=g.planets[i].resources;
+		BotSet total_we, total_they;
+		for(auto wg:g.planets[i].workerGroups){
+			(is_player_evil(wg.playerIndex)?total_they:total_we)[get_specialty(wg.playerIndex)]+=wg.number;
+		}
+		tie(total_we, total_they)=battle(total_we, total_they);
 	}
 }
 void GW::init(){
+	ourBots.resize(game->planets.size());
+	theirBots.resize(game->planets.size());
+	resource.resize(game->planets.size());
 	init_graph(*game);
 }
 int GW::get_tick(){
@@ -37,7 +55,7 @@ int GW::get_my_index(){
 	return game->myIndex;
 }
 int GW::get_our_team(){
-	return game->players[get_my_index()].teamIndex;
+	return get_team(get_my_index());
 }
 int GW::get_our_score(){
 	return ourScore;
@@ -46,18 +64,28 @@ int GW::get_their_score(){
 	return theirScore;
 }
 bool GW::is_player_evil(int idx){
-	return get_our_team()-game->players[idx].teamIndex;
+	return get_our_team()-get_team(idx);
 }
 bool GW::is_player_good(int idx){
 	return !is_player_evil(idx);
 }
+Specialty GW::get_specialty(int idx){
+	assert((g.players[idx].specialty)&&"The specialty must exits. Use this function only if tick>=3");
+	mySpecialty=g.players[idx].specialty.value();
+}
 Specialty GW::get_my_specialty(){
-	return mySpecialty;
+	return get_specialty(get_my_index());
 }
 int GW::who_can(Specialty s){
 	return whoCan[s];
 }
 int GW::who_can_evil(Specialty s){
 	return whoCanEvil[s];
+}
+int GW::get_team(int idx){
+	return game->players[idx].teamIndex;
+}
+pair<BotSet, BotSet> battle(BotSet a, BotSet b){
+	return {a, b}; // TODO Real battle so on one of BotSets will be 0,0,0
 }
 #undef GW
--- strategy/GameWrapper.h
+++ strategy/GameWrapper.h
@@ -7,9 +7,13 @@ class GameWrapper{
 		void init();
 		vi availFly;
 		int ourScore, theirScore;
-		Specialty mySpecialty;
 		dict<Specialty, int> whoCan;
 		dict<Specialty, int> whoCanEvil;
+		BotSet ourFlyingBots;
+		BotSet theirFlyingBots;
+		vector<BotSet> ourBots;
+		vector<BotSet> theirBots;
+		vector<ResSet> resources;
 	public:
 		GameWrapper();
 		void update(const Game&);
@@ -25,4 +29,11 @@ class GameWrapper{
 		bool is_player_evil(int idx); // Their team's player
 		int who_can(Specialty); // Returns who from our team has such specialty
 		int who_can_evil(Specialty); // Returns who from their team has such specialty
+		Specialty get_specialty(int idx); // Returns specialty of playert with index == idx;
+		// reserve_*_bot works like this: taking bots from planet and return them to you. If there is not enough bots then take some of them. Priority is for fast: FAST, WARRIORS, WORKERS. For work: WORKERS, WARRIORS, FAST. TODO rewrite reservation: if we want to move and there are not availFly, reserve anather and so on.
+		BotSet reserve_fast_bot(int);
+		BotSet reserve_work_bot(int);
+		void reserve_res(Resource, int);
+		int get_team(int);// Returns team by player index
+		pair<BotSet, BotSet> battle(BotSet a, BotSet b); // Кто выживет.
 };

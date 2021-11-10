7ce8eae6674027a16bfb3a302a5b13fddecbd0b1
SerZak05 <serzakand@gmail.com>
2021-11-10 16:58:35 +0300

    Протестировал, удалил ненужный вывод. Программа начинает действовать с первого тика.

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -9,9 +9,6 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 
 	game_wrapper.update(game);
 
-	/*if (game.currentTick <= 3) // FIXME
-		return Action(); // TODO: return my specialty*/
-
 	core.process(game_wrapper);
 
 	Action result;
@@ -23,9 +20,5 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 		}
 	}
 	result.chooseSpecialty = game_wrapper.getNextFreeSpecialty();
-	std::cerr << "Speciality of #" << game_wrapper.getMyPlayerId() << " is " << (int)result.chooseSpecialty.value() << std::endl;
-	/*if (game_wrapper.getMySpecialty() != game_wrapper.getPlannedSpeciality(game_wrapper.getMyPlayerId())) {
-		std::cerr << "Cry" << std::endl;
-	}*/
 	return result;
 }
